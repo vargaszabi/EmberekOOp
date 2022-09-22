7d076cc802dfbc77dd1ede4351473c25c9ec7264
vargaszabi <vargaszabi2001@gmail.com>
2022-09-22 17:21:07 +0200

    Emberek osztály -tostring -példányosítás kiiratás

--- src/hu/petrik/emberekoop/Emberek.java
+++ src/hu/petrik/emberekoop/Emberek.java
@@ -11,4 +11,13 @@ public class Emberek {
         this.emberek = new ArrayList<>();
         this.emberek.addAll(Arrays.asList(emberTomb));
     }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder(String.format("%30s %10s %20s (%3s év)","Név", "Szül.dátum","Szül.hely", "Kor" ));
+        for (int i = 0; i<this.emberek.size(); i++){
+            s.append(System.lineSeparator()).append(this.emberek.get(i));
+        }
+        return s.toString();
+    }
 }
--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -2,11 +2,13 @@ package hu.petrik.emberekoop;
 
 public class Main {
     public static void main(String[] args) {
-        Ember e1 = new Ember("Ol Ajos","2000-05-06", "Bivalybasznád");
-        Ember e2 = new Ember("Maki Verem","2003-12-25", "Budapest");
-        Ember e3 = new Ember("Kovács István","1998-08-01", "Debrecen");
-        System.out.println(e1);
-        System.out.println(e2);
-        System.out.println(e3);
+        Ember e1 = new Ember("Ol Ajos", "2000-05-06", "Bivalybasznád");
+        Ember e2 = new Ember("Maki Verem", "2003-12-25", "Budapest");
+        Ember e3 = new Ember("Kovács István", "1998-08-01", "Debrecen");
+        Ember e4 = new Ember("Kis Abdul", "2008-12-30", "Veszprém");
+        Ember e5 = new Ember("Lakatos Mercédesz", "1998-03-5", "Pécs");
+        Ember[] emberTomb = new Ember[]{e1, e2, e3, e4, e5};
+        Emberek ek = new Emberek(emberTomb);
+        System.out.println(ek);
     }
 }
