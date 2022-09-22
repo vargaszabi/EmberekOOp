3757d71efe33fcac5bfc2cf8570a3bfb1cf3431e
vargaszabi <vargaszabi2001@gmail.com>
2022-09-22 16:20:45 +0200

    Ember -Dátum különszedve

--- src/hu/petrik/emberekoop/Ember.java
+++ src/hu/petrik/emberekoop/Ember.java
@@ -10,9 +10,20 @@ public class Ember {
         this.szulDatum = szulDatum;
         this.szulHely = szulHely;
     }
+    public int getSzuletesiEv(){
+        return Integer.parseInt(this.szulDatum.substring(0,4));
+    }
+    public int getSzulHonap(){
+        String[] szuletesiAdatok = this.szulDatum.split("-");
+        return Integer.parseInt(szuletesiAdatok[1]);
+    }
+    public int getSzulNap(){
+        String[] szuletesiAdatok = this.szulDatum.split("-");
+        return Integer.parseInt(szuletesiAdatok[2]);
+    }
 
     @Override
     public String toString() {
-        return String.format("%30s %10s %20s", this.nev, this.szulDatum,this.szulHely);
+        return String.format("%30s %10s %20s %d %d", this.nev, this.szulDatum,this.szulHely, this.getSzulHonap(), this.getSzulNap());
     }
 }
