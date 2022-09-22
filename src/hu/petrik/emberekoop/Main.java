012cf0cc9f848c3d2bd1ccb709754065640551ab
vargaszabi <vargaszabi2001@gmail.com>
2022-09-22 16:05:34 +0200

    Ember osztály -adattagok -paraméteres konstruktor -tostring Main -3 emberpéldány létrehozása és kiiratása

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -1,4 +1,12 @@
 package hu.petrik.emberekoop;
 
 public class Main {
+    public static void main(String[] args) {
+        Ember e1 = new Ember("Ol Ajos","2000-05-06", "Bivalybasznád");
+        Ember e2 = new Ember("Maki Verem","2003-12-25", "Budapest");
+        Ember e3 = new Ember("Kovács István","1998-08-01", "Debrecen");
+        System.out.println(e1);
+        System.out.println(e2);
+        System.out.println(e3);
+    }
 }
